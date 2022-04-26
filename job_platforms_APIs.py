d8bdeb07392286cec8507f72c30bed9400bd65e2
Roman <graff049@gmail.com>
2022-04-26 21:40:19 +0300

    add module for APIs

---
+++ job_platforms_APIs.py
@@ -0,0 +1,10 @@
+
+
+class BaseApi:
+    """Base API class which implements with all repeatable attributes and methods."""
+
+    def __init__(self, base_url: str, headers: Dict):
+        self.base_url = base_url
+        self.headers = headers
+        self.session = requests.Session()
+        self.session.headers.update(self.headers)
