e05048222b360e9a5d7d74f1a98f6abcc0cdd7bc
Roman <graff049@gmail.com>
2022-04-26 22:17:23 +0300

    1. create HeadHunterApi class 2. add get_access_token

--- job_platforms_APIs.py
+++ job_platforms_APIs.py
@@ -9,3 +9,23 @@ class BaseApi:
         self.headers = headers
         self.session = requests.Session()
         self.session.headers.update(self.headers)
+
+
+class HeadHunterApi(BaseApi):
+
+    def get_access_token(self, client_id: str, client_secret: str):
+        endpoint = 'oauth/token'
+        url = f'https://hh.ru/{endpoint}'
+        params = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'client_credentials',
+        }
+        access_token = self.session.post(url=url, params=params)
+        access_token.raise_for_status()
+        return access_token
+
+
+if __name__ == '__main__':
+    hh_api = HeadHunterApi(base_url='https://api.hh.ru/', headers={})
+    access_token = hh_api.get_access_token(client_id=client_id, client_secret=client_secret)
