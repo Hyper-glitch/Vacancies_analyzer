1ccfd14677643d9b84f2cc7bc0afb9aed9343e37
Roman <graff049@gmail.com>
2022-04-26 21:50:44 +0300

    import requests

--- job_platforms_APIs.py
+++ job_platforms_APIs.py
@@ -1,9 +1,10 @@
+import requests
 
 
 class BaseApi:
     """Base API class which implements with all repeatable attributes and methods."""
 
-    def __init__(self, base_url: str, headers: Dict):
+    def __init__(self, base_url: str, headers: dict):
         self.base_url = base_url
         self.headers = headers
         self.session = requests.Session()
