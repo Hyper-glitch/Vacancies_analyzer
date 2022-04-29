2d213b17c467f46605bb96ad193039982b9dc3ea
Roman <graff049@gmail.com>
2022-04-29 21:02:50 +0300

    1. add get_json 2. add get_authorization_code 3. get_vacancies methods

--- job_platforms_APIs.py
+++ job_platforms_APIs.py
@@ -1,4 +1,8 @@
+import urllib.parse as urllib
+
 import requests
+
+from apis_constants import HH_HEADERS, HH_BASE_API_URL
 
 
 class BaseApi:
@@ -10,8 +14,28 @@ class BaseApi:
         self.session = requests.Session()
         self.session.headers.update(self.headers)
 
+    def get_json(self, endpoint: str, params: dict = None) -> dict:
+        """Make full url and send GET request.
+        :param endpoint: - it is a point to interact with definite method to API
+        :param params: - additional data for getting info from response
+        :returns: response.json() - information from response in dict data structure
+        """
+        url = urllib.urljoin(self.base_url, endpoint)
+        response = self.session.get(url=url, params=params)
+        response.raise_for_status()
+        return response.json()
+
 
 class HeadHunterApi(BaseApi):
+
+    def get_authorization_code(self):
+        endpoint = 'oauth/authorize'
+        url = f'https://hh.ru/{endpoint}'
+        params = {
+            'response_type': 'code',
+            'client_id': 83918056,
+        }
+        authorization_code = self.session.post(url=url, params=params)
 
     def get_access_token(self, client_id: str, client_secret: str):
         endpoint = 'oauth/token'
@@ -25,7 +49,12 @@ class HeadHunterApi(BaseApi):
         access_token.raise_for_status()
         return access_token
 
+    def get_vacancies(self, query):
+        endpoint = f'vacancies/{query}'
+        vacancies = self.get_json(endpoint=endpoint)
+        return vacancies
+
 
 if __name__ == '__main__':
-    hh_api = HeadHunterApi(base_url='https://api.hh.ru/', headers={})
-    access_token = hh_api.get_access_token(client_id=client_id, client_secret=client_secret)
+    hh_api = HeadHunterApi(base_url=HH_BASE_API_URL, headers=HH_HEADERS)
+    developer_vacancies = hh_api.get_authorization_code()
