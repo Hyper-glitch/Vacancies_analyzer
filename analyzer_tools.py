16311fcc439c25c726a23877a01525f53395527a
Roman <graff049@gmail.com>
2022-05-10 14:43:37 +0300

    1. make predict_rub_salary universal for both job platforms

--- analyzer_tools.py
+++ analyzer_tools.py
@@ -2,35 +2,22 @@
 from terminaltables import AsciiTable
 
 
-def predict_salary(currency, start_salary, end_salary, currency_name):
-    expected_salary = None
-
-    if currency != currency_name:
-        return
-    if start_salary and end_salary:
-        expected_salary = (start_salary + end_salary) / 2
-    elif start_salary:
-        expected_salary = start_salary * 1.2
-    elif not start_salary and end_salary:
-        expected_salary = end_salary * 0.8
-
-    return expected_salary
-
-
 def get_analyzed_vacancies(api, programming_languages: list, search_key: str, vacancies_params: dict):
     analyzed_language_vacancies = {}
-    super_job_indicator = 'keyword'
+    super_job_search_key = 'keyword'
+    super_job_indicator = False
 
     for language in programming_languages:
         search_text = f'Программист {language}'
         vacancies_params.update({search_key: search_text})
 
-        if search_key == super_job_indicator:
+        if search_key == super_job_search_key:
             all_vacancies = api.get_vacancies(params=vacancies_params)
+            super_job_indicator = True
         else:
             all_vacancies = api.get_all_vacancies(params=vacancies_params)
 
-        expected_salaries = api.predict_rub_salary(all_vacancies)
+        expected_salaries = predict_rub_salary(all_vacancies, super_job_indicator)
         vacancies_found = len(all_vacancies)
         vacancies_processed = len(expected_salaries)
 
@@ -58,3 +45,48 @@ def show_vacancies_statistics(analyzed_vacancies, title):
 
     hh_table = AsciiTable(table_data=table_data, title=title)
     print(hh_table.table)
+
+
+def predict_salary(currency, start_salary, end_salary, currency_name):
+    expected_salary = None
+
+    if currency != currency_name:
+        return
+    if start_salary and end_salary:
+        expected_salary = (start_salary + end_salary) / 2
+    elif start_salary:
+        expected_salary = start_salary * 1.2
+    elif not start_salary and end_salary:
+        expected_salary = end_salary * 0.8
+
+    return expected_salary
+
+
+def predict_rub_salary(vacancies, super_job_indicator):
+    expected_salaries = []
+
+    for vacancy in vacancies:
+        if not super_job_indicator:
+            salary = vacancy['salary']
+
+            if not salary:
+                continue
+
+            currency = salary['currency']
+            start_salary = salary['from']
+            end_salary = salary['to']
+            currency_name = 'RUR'
+        else:
+            currency = vacancy['currency']
+            start_salary = vacancy['payment_from']
+            end_salary = vacancy['payment_to']
+            currency_name = 'rub'
+
+        expected_salary = predict_salary(
+            currency=currency, start_salary=start_salary,
+            end_salary=end_salary, currency_name=currency_name
+        )
+        if expected_salary:
+            expected_salaries.append(expected_salary)
+
+    return expected_salaries
